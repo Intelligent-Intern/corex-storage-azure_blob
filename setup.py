848d3c2560900759dd4214571c1d5daf95bd01be
Jochen Schultz <jschultz@php.net>
2025-03-30 16:26:15 +0200

    Initial commit with GitHub Action workflow

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="corex-storage-azure_blob",
+    version="0.1.0",
+    author="Jochen Schultz",
+    author_email="js@intelligent-intern.com",
+    packages=["corex_storage_azure_blob"],
+    install_requires=[],
+    python_requires=">=3.7",
+)
